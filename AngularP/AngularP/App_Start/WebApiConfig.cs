4a574380d4eb582764e1ab3977a6e7d5d9ebc147
Reut <80757976+rupi462021@users.noreply.github.com>
2021-05-03 17:23:18 +0300

    first commit

---
+++ AngularP/AngularP/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AngularP
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var cors = new EnableCorsAttribute(
+
+            "*", // allowed websites
+
+            "*", // allowed headers
+
+            "*"); // allowed methods
+
+            config.EnableCors(cors);
+            // JSON camel case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
